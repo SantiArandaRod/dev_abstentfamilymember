60d5d61068e35bd4659ff90677dc0ae4217ae47e
dsar <dsaranda89@ucatolica.edu.co>
2025-07-04 23:16:44 -0500

    add one model for update profile

--- modelsSQL.py
+++ modelsSQL.py
@@ -1,4 +1,3 @@
-import sqlite3
 
 from sqlmodel import SQLModel,Field
 from pydantic import ConfigDict
@@ -9,10 +8,17 @@ class ProfileBase(SQLModel):
     Name: str= Field(..., min_length=1,max_length=100)
     Relationship: str= Field(..., min_length=1,max_length=100)
     Message: str= Field(..., min_length=1,max_length=5000)
-    image_path: str= Field(default=None)
+    image_path: Optional[str] = None
     confirmation: bool= Field(default=False)
 
-class ProfileSQL(ProfileBase):
-    __tablename__= "profiles"
+class ProfileSQL(ProfileBase, table=True):
+    __tablename__ = "profiles"
     id: Optional[int] = Field(default=None, primary_key=True)
     model_config = ConfigDict(from_attributes=True)
+
+class ProfileUpdate(SQLModel):
+    Name: Optional[str] = None
+    Relationship: Optional[str] = None
+    Message: Optional[str] = None
+    image_path: Optional[str] = None
+    confirmation: Optional[bool] = None
